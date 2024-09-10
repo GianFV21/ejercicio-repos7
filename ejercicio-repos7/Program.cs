06c297028d697cb9c4ad465c7876ff5fe2130e4b
Flores V�squez Carlos Gianfranco <FV23003@ues.edu.sv>
2024-09-10 17:52:52 -0600

    Ingreso de variables num1,Num2

--- ejercicio-repos7/Program.cs
+++ ejercicio-repos7/Program.cs
@@ -10,6 +10,12 @@ namespace ejercicio_repos7
     {
         static void Main(string[] args)
         {
+            Double Num1,Num2;
+            Console.WriteLine("Ingrese el numero 1: ");
+            Num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el numero 2: ");
+            Num2 = Double.Parse(Console.ReadLine());
+
         }
     }
 }
